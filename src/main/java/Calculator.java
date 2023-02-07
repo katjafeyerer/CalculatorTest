cd7f814800e1d75380e83ee92199e43a1dddb362
katja.feyerer <katja.feyerer@gmail.com>
2023-02-07 21:48:24 +0100

    Calculator created Calculator Test written Implementation finished

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,36 @@
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    int a = 4;
+    int b = 3;
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        int expected = 7;
+        Assertions.assertEquals(expected, Calculator.add(a, b));
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+        Assertions.assertEquals(1, Calculator.subtract(a, b));
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+        Assertions.assertEquals(12, Calculator.multiply(a, b));
+    }
+}
